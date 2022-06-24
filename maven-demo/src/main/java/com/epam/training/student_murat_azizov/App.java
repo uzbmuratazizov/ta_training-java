975e9cf2cedc0cdaf950ed644176ba357f8b0dbb
Murat Azizov <4e.murod@gmail.com>
2022-06-24 23:36:34 +0500

    First commit. Task 1.

---
+++ maven-demo/src/main/java/com/epam/training/student_murat_azizov/App.java
@@ -0,0 +1,14 @@
+package com.epam.training.student_murat_azizov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
